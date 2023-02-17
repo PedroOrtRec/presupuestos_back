7a74a9f6d97e2c6538e741ae0d1837c745f97158
PedroOrtRec <pedro.ortega.dw@immune.institute>
2023-02-18 00:04:35 +0100

    Agregado modelo para modificar usuarios

--- models/users.model.js
+++ models/users.model.js
@@ -10,7 +10,14 @@ const createUser = ({ userName, userEmail, password }) => {
     return db.query('INSERT INTO users (userName, userEmail, password) VALUES (?, ?, ?)', [userName, userEmail, password])
 }
 
+const updateUser = (userId, { userName, userEmail, password }) => {
+    return db.query(
+        'UPDATE users SET userName = ?, userEmail = ?, password = ? WHERE userId = ?',
+        [userName, userEmail, password, userId]
+    )
+}
+
 
 module.exports = {
-    getUserById, createUser, getUserByEmail
+    getUserById, createUser, getUserByEmail, updateUser
 }
