57c45f5d6926b53c9bd362adfcf634f8d8972cc1
PedroOrtRec <pedro.ortega.dw@immune.institute>
2023-02-17 23:56:41 +0100

    Request de user por email. creación de fichero para modelos de grupos

--- models/users.model.js
+++ models/users.model.js
@@ -2,11 +2,15 @@ const getUserById = (userId) => {
     return db.query('SELECT * FROM users WHERE userId = ?', [userId])
 }
 
+const getUserByEmail = (userEmail) => {
+    return db.query('SELECT * FROM users WHERE userEmail = ?', [userEmail])
+}
+
 const createUser = ({ userName, userEmail, password }) => {
     return db.query('INSERT INTO users (userName, userEmail, password) VALUES (?, ?, ?)', [userName, userEmail, password])
 }
 
 
 module.exports = {
-    getUserById, createUser
+    getUserById, createUser, getUserByEmail
 }
