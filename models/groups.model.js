8ef8732adfae3ea3d10b5dda2fd151c5289e8fe5
PedroOrtRec <pedro.ortega.dw@immune.institute>
2023-02-19 10:21:00 +0100

    modelo para obtener grupos a través de la id del usuario

--- models/groups.model.js
+++ models/groups.model.js
@@ -1,3 +1,7 @@
-// const getGroupsByUserId = () {
+const getGroupsByUserId = (userId) => {
+    return db.query('SELECT g.groupId, g.groupTitle, g.groupDescription, g.creationTime FROM dbgroups AS g INNER JOIN  groups_has_users AS tbi ON g.groupId = tbi.groupId INNER JOIN users AS u ON tbi.userId = u.userId WHERE u.userId = ?', [userId])
+}
 
-// }
+module.exports = {
+    getGroupsByUserId
+}
